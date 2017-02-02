9f512bc2162a551035fa700926922feee633cab2
Tarjei E. Ormestøyl <tarjeieo@puzzlepart.com>
2017-02-02 22:05:23 +0100

    Removed local build files from source

--- node_modules/office-ui-fabric-react/lib/demo/pages/DropdownPage/examples/Dropdown.Basic.Example.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import './Dropdown.Basic.Example.scss';
-export declare class DropdownBasicExample extends React.Component<any, any> {
-    render(): JSX.Element;
-}
